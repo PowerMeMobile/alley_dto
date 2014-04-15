385544275a4222c598b87f50dbb439265823ff12
Dmitry Klionsky <dm.klionsky@gmail.com>
2014-04-15 14:18:09 +0300

    Cleanup

--- src/adto.erl
+++ src/adto.erl
@@ -35,47 +35,47 @@ decode(Type, Message) ->
 
 %% Funnel dto
 module(FunnelDTO) when
-            is_record(FunnelDTO, funnel_auth_request_dto)
-    orelse  is_record(FunnelDTO, funnel_auth_response_dto)
-    orelse  is_record(FunnelDTO, funnel_started_event_dto)
-    orelse  is_record(FunnelDTO, funnel_stopped_event_dto)
-    orelse  is_record(FunnelDTO, funnel_client_online_event_dto)
-    orelse  is_record(FunnelDTO, funnel_client_offline_event_dto)
-    orelse  is_record(FunnelDTO, funnel_incoming_sms_dto)
-    orelse  is_record(FunnelDTO, funnel_delivery_receipt_dto)
-    orelse  is_record(FunnelDTO, funnel_ack_dto)
-    orelse  is_record(FunnelDTO, funnel_connections_request_dto)
-    orelse  is_record(FunnelDTO, funnel_connections_response_dto) ->
+           is_record(FunnelDTO, funnel_auth_request_dto)
+    orelse is_record(FunnelDTO, funnel_auth_response_dto)
+    orelse is_record(FunnelDTO, funnel_started_event_dto)
+    orelse is_record(FunnelDTO, funnel_stopped_event_dto)
+    orelse is_record(FunnelDTO, funnel_client_online_event_dto)
+    orelse is_record(FunnelDTO, funnel_client_offline_event_dto)
+    orelse is_record(FunnelDTO, funnel_incoming_sms_dto)
+    orelse is_record(FunnelDTO, funnel_delivery_receipt_dto)
+    orelse is_record(FunnelDTO, funnel_ack_dto)
+    orelse is_record(FunnelDTO, funnel_connections_request_dto)
+    orelse is_record(FunnelDTO, funnel_connections_response_dto) ->
     funnel();
 
 %% Just dto
 module(JustDTO) when
-            is_record(JustDTO, just_sms_request_dto)
-    orelse  is_record(JustDTO, just_sms_response_dto)
-    orelse  is_record(JustDTO, just_incoming_sms_dto)
-    orelse  is_record(JustDTO, just_delivery_receipt_dto) ->
+           is_record(JustDTO, just_sms_request_dto)
+    orelse is_record(JustDTO, just_sms_response_dto)
+    orelse is_record(JustDTO, just_incoming_sms_dto)
+    orelse is_record(JustDTO, just_delivery_receipt_dto) ->
     just();
 
 %% k1api dto
 module(K1apiDTO) when
-            is_record(K1apiDTO, k1api_auth_request_dto)
-    orelse  is_record(K1apiDTO, k1api_auth_response_dto)
-    orelse  is_record(K1apiDTO, k1api_sms_delivery_status_request_dto)
-    orelse  is_record(K1apiDTO, k1api_sms_delivery_status_response_dto)
-    orelse  is_record(K1apiDTO, k1api_retrieve_sms_request_dto)
-    orelse  is_record(K1apiDTO, k1api_retrieve_sms_response_dto)
-    orelse  is_record(K1apiDTO, k1api_remove_retrieved_sms_request_dto)
-    orelse  is_record(K1apiDTO, k1api_subscribe_incoming_sms_request_dto)
-    orelse  is_record(K1apiDTO, k1api_subscribe_incoming_sms_response_dto)
-    orelse  is_record(K1apiDTO, k1api_unsubscribe_incoming_sms_request_dto)
-    orelse  is_record(K1apiDTO, k1api_unsubscribe_incoming_sms_response_dto)
-    orelse  is_record(K1apiDTO, k1api_sms_notification_request_dto)
-    orelse  is_record(K1apiDTO, k1api_subscribe_sms_receipts_request_dto)
-    orelse  is_record(K1apiDTO, k1api_subscribe_sms_receipts_response_dto)
-    orelse  is_record(K1apiDTO, k1api_unsubscribe_sms_receipts_request_dto)
-    orelse  is_record(K1apiDTO, k1api_unsubscribe_sms_receipts_response_dto)
-    orelse  is_record(K1apiDTO, k1api_sms_delivery_receipt_notification_dto)
-    orelse  is_record(K1apiDTO, k1api_coverage_response_dto) ->
+           is_record(K1apiDTO, k1api_auth_request_dto)
+    orelse is_record(K1apiDTO, k1api_auth_response_dto)
+    orelse is_record(K1apiDTO, k1api_sms_delivery_status_request_dto)
+    orelse is_record(K1apiDTO, k1api_sms_delivery_status_response_dto)
+    orelse is_record(K1apiDTO, k1api_retrieve_sms_request_dto)
+    orelse is_record(K1apiDTO, k1api_retrieve_sms_response_dto)
+    orelse is_record(K1apiDTO, k1api_remove_retrieved_sms_request_dto)
+    orelse is_record(K1apiDTO, k1api_subscribe_incoming_sms_request_dto)
+    orelse is_record(K1apiDTO, k1api_subscribe_incoming_sms_response_dto)
+    orelse is_record(K1apiDTO, k1api_unsubscribe_incoming_sms_request_dto)
+    orelse is_record(K1apiDTO, k1api_unsubscribe_incoming_sms_response_dto)
+    orelse is_record(K1apiDTO, k1api_sms_notification_request_dto)
+    orelse is_record(K1apiDTO, k1api_subscribe_sms_receipts_request_dto)
+    orelse is_record(K1apiDTO, k1api_subscribe_sms_receipts_response_dto)
+    orelse is_record(K1apiDTO, k1api_unsubscribe_sms_receipts_request_dto)
+    orelse is_record(K1apiDTO, k1api_unsubscribe_sms_receipts_response_dto)
+    orelse is_record(K1apiDTO, k1api_sms_delivery_receipt_notification_dto)
+    orelse is_record(K1apiDTO, k1api_coverage_response_dto) ->
     k1api();
 
 module(Type) ->
@@ -83,5 +83,5 @@ module(Type) ->
 
 %% codec modules
 funnel() -> adto_funnel.
-just() ->   adto_just.
-k1api() ->  adto_k1api.
+just()   -> adto_just.
+k1api()  -> adto_k1api.
